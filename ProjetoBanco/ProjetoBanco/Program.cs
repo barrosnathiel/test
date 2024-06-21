7cf5ae7e4e8b21b9bfc9fc954f8818a827851ed5
BarrosNathiel <BarrosNathiel@users.noreply.github.com>
2024-06-20 21:58:53 -0300

    Novo update

--- ProjetoBanco/ProjetoBanco/Program.cs
+++ ProjetoBanco/ProjetoBanco/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Código Main");
+Console.WriteLine("Código Main ooooo"); Console.WriteLine("Código Update ooooo");
 Console.WriteLine("Código Backend");
