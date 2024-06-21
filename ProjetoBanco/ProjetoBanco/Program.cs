5ddc8b3b2ecfdae17dbe73bdc87373ff6c647681
BarrosNathiel <BarrosNathiel@users.noreply.github.com>
2024-06-20 21:59:50 -0300

    Alteracao emergencial

--- ProjetoBanco/ProjetoBanco/Program.cs
+++ ProjetoBanco/ProjetoBanco/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Código Main");
+Console.WriteLine("Código Main"); Console.WriteLine("Código Main");
 Console.WriteLine("Código Backend");
